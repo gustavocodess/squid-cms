ce5fbe28e097d64b47382ef224716da3bc5e7010
Gustavo Henrique Garcia Silva <gustavocodes@outlook.com>
2019-05-20 01:29:40 -0300

    added some empty state to dashboard screenn

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Lottie from 'react-lottie'
 // import PropTypes from 'prop-types'
+
+const squidLoading = require('../../assets/animations/construction.json')
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: squidLoading,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+}
 
 const Dashboard = () => (
   <div>
     <h2>Dashboard</h2>
+    <div style={{
+      display: 'flex', alignItems: 'center', flexDirection: 'column', width: '100%', height: '50vh',
+    }}
+    >
+      <Lottie
+        options={defaultOptions}
+        height={300}
+        width={400}
+      />
+      <span style={{ fontFamily: 'GloberSemiBold' }}>Work in progress...</span> 
+    </div>
   </div>
 )
 
